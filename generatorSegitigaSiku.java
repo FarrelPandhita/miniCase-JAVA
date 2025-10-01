2a387ee2ba8b103b4fe5ce6af07b5155427aaa9a
h0lys41nt <121160077+FarrelPandhita@users.noreply.github.com>
2025-10-01 21:10:41 +0700

    Create generatorSegitigaSiku.java

---
+++ generatorSegitigaSiku.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class generatorSegitigaSiku {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai n: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  "); // Cetak dua spasi agar sejajar dengan "* "
+            }
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            // Pindah ke baris baru setelah satu baris selesai
+            System.out.println();
+        }
+
+
+
+    }
+}
